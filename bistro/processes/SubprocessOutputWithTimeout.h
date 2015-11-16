88972630b6753190beeb94c4f53dfb17a764c9b2
Yuri Putivsky <yuri@fb.com>
2015-11-16 09:47:05 -0800

    Added command line blocking execution with timeout and stdout, stderr support
    Summary:
    In order to get GPU resources we are going to use nvidia-smi command line tool.
    To prevent hangs or other problems AsyncSubrpocess class gets employed.
    Information from nvidia-smi output collected into array of strings for futher processing.
    
    Reviewed By: snarkmaster
    
    Differential Revision: D2650090
    
    fbshipit-source-id: 2af9a1781f88bf4ff24f78e43b5f32925563ce84

---
+++ bistro/processes/SubprocessOutputWithTimeout.h
@@ -0,0 +1,35 @@
+/*
+ *  Copyright (c) 2015, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+#pragma once
+
+#include "bistro/bistro/processes/AsyncSubprocess.h"
+
+namespace facebook { namespace bistro {
+
+/**
+ * Waits (blocking call) for subprocess completion, returns subprocess return
+ * code, stdout and stderr lines. Newlines are preserved, lines longer than 2048
+ * characters are broken up into adjacent "lines" with no trailing newlines
+ * Caller provides command line (array of strings) and timeout.
+ * Important!!!
+ * If subprocess opens a next generation of subprocesses with the inherited
+ * stderr/stdout FDs and some level of independence, i.e detach themselves from
+ * the process group and keep the stderr/stdout FDs open then timeout won't
+ * work, because function will be waiting for stderr/stdout FDs get closed.
+ * In such case function would hang up till life time of sub-subprocesses.
+ */
+folly::ProcessReturnCode subprocessOutputWithTimeout(
+    const std::vector<std::string>& cmd,
+    std::vector<std::string>* stdOutLines,
+    std::vector<std::string>* stdErrLines,
+    uint32_t timeoutMs,
+    uint32_t pollMs = 10);
+
+}}  // namespace facebook::bistro

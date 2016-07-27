8a6ed181613db277d80f54d283e8a7220352422c
Andrey Goder <agoder@fb.com>
2016-07-27 15:10:35 -0700

    fix build
    Summary: This header was missing
    
    Reviewed By: snarkmaster
    
    Differential Revision: D3630517
    
    fbshipit-source-id: 991491996daa00a3bd0d71ea5b6a569cf7727491

--- bistro/processes/AsyncReadPipe.h
+++ bistro/processes/AsyncReadPipe.h
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2015, Facebook, Inc.
+ *  Copyright (c) 2016, Facebook, Inc.
  *  All rights reserved.
  *
  *  This source code is licensed under the BSD-style license found in the
@@ -10,6 +10,7 @@
 #pragma once
 
 #include <folly/File.h>
+#include <folly/Exception.h>
 #include <folly/FileUtil.h>
 #include <folly/futures/Future.h>
 #include <folly/futures/Promise.h>

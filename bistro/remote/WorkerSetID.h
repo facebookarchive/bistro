74c511d2c4194e16666ecb95367be5ca4d5eaff3
Alexey Spiridonov <lesha@fb.com>
2016-08-10 14:48:05 -0700

    Fix opt-asan implementation-defined conversion bug
    Summary: C++ is the worst.
    
    Reviewed By: agoder
    
    Differential Revision: D3679849
    
    fbshipit-source-id: bb1332a807756b4375dbb20295019cb2a5bd5870

--- bistro/remote/WorkerSetID.h
+++ bistro/remote/WorkerSetID.h
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2015, Facebook, Inc.
+ *  Copyright (c) 2016, Facebook, Inc.
  *  All rights reserved.
  *
  *  This source code is licensed under the BSD-style license found in the
@@ -8,6 +8,8 @@
  *
  */
 #pragma once
+
+#include <limits>
 
 #include "bistro/bistro/if/gen-cpp2/common_types.h"
 
@@ -49,10 +51,24 @@ inline void removeWorkerIDFromHash(
  * a static assert in case we're not compiled on a two's complement box.
  */
 struct WorkerSetIDEarlierThan {
-  bool operator()(int64_t a, int64_t b) const {
-    if (a - b >= 0) {
+  // The C++ standard does not define how unsigned -> signed conversion must
+  // proceed, so use 2's complement just like this logic needs.
+  int64_t makeSigned(uint64_t v) const {
+    return (v < 0x8000000000000000) ? v : (
+      std::numeric_limits<int64_t>::min() + (v - 0x8000000000000000)
+    );
+  }
+  // Take unsigned arguments because signed => unsigned implicit conversion
+  // is well-defined by C++, but the opposite direction isn't... and we need
+  // unsigned ints below for the subtraction anyway.
+  //   http://en.cppreference.com/w/cpp/language/implicit_conversion
+  bool operator()(uint64_t a, uint64_t b) const {
+    // The C++ fails to specify signed overflow behavior, and GCC + ASAN +
+    // optimizations actually seems to break some of our unit tests, so do
+    // an unsigned subtraction and then coerce according to 2's complement.
+    if (makeSigned(a - b) >= 0) {
       return false;
-    } else if (b - a > 0) {
+    } else if (makeSigned(b - a) > 0) {
       return true;
     }
     LOG(FATAL) << "Versions differ by 2^63: a = " << a << ", b = " << b;

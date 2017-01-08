2b23a6d03cf0ea6e2d4ec3c3ad3aa5b5f74a0ce4
Igor Sugak <sugak@fb.com>
2017-01-08 12:55:27 -0800

    use boost/serialization/strong_typedef.hpp instead of boost/strong_typedef.hpp
    Summary:
    Looks like `boost/strong_typedef.hpp` was deprecated for a long time. In 1.63.0 it was finally removed. The current active boost-1.57.0 was shipped with both headers, making this diff backward compatible.
    
    http://stackoverflow.com/questions/16901250/why-does-boost-include-two-different-versions-of-strong-typedef-hpp
    
    ```
    fbgs -ls 'boost/strong_typedef.hpp' | xargs perl -pi -e 's/boost\/strong_typedef\.hpp/boost\/serialization\/strong_typedef\.hpp/g'
    ```
    
    Reviewed By: yfeldblum
    
    Differential Revision: D4391433
    
    fbshipit-source-id: ba7cb2de74961de5a6f58cd500e21e6e884efe30

--- bistro/config/Job.h
+++ bistro/config/Job.h
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2016, Facebook, Inc.
+ *  Copyright (c) 2017, Facebook, Inc.
  *  All rights reserved.
  *
  *  This source code is licensed under the BSD-style license found in the
@@ -9,7 +9,7 @@
  */
 #pragma once
 
-#include <boost/strong_typedef.hpp>
+#include <boost/serialization/strong_typedef.hpp>
 #include <folly/Optional.h>
 #include <functional>
 #include <memory>
--- bistro/config/Node.h
+++ bistro/config/Node.h
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2015, Facebook, Inc.
+ *  Copyright (c) 2017, Facebook, Inc.
  *  All rights reserved.
  *
  *  This source code is licensed under the BSD-style license found in the
@@ -11,7 +11,7 @@
 
 #include <boost/iterator/iterator_facade.hpp>
 #include <boost/range/iterator_range.hpp>
-#include <boost/strong_typedef.hpp>
+#include <boost/serialization/strong_typedef.hpp>
 #include <folly/Synchronized.h>
 #include <folly/sorted_vector_types.h>
 #include <memory>

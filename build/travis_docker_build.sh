300e6f90c3a94b7838a94eb786be4e329425acd1
lesha <lesha@fb.com>
2017-02-09 19:11:41 -0800

    Part 1 of eliminating most of the per-project boilerplate
    Summary:
    I'm pushing a lot more of the docs and logic that can be shared between projects into `fbcode_builder`.
    
    On the ShipIt side, I created a filter that hides all the details of `fbcode_builder` setup from the project owner, so that adding the filter is now a 5-line diff.
    
    Up next: eliminating the 3 remaining per-project scripts that I have in Bistro, and folding them into `fbcode_builder`.
    
    Reviewed By: sdwilsh
    
    Differential Revision: D4474207
    
    fbshipit-source-id: ea27a537e46baad004e898736127fe52dc8056cd

--- build/travis_docker_build.sh
+++ build/travis_docker_build.sh
@@ -4,12 +4,15 @@
 os_image=${os_image?Must be set by Travis}
 gcc_version=${gcc_version?Must be set by Travis}
 make_parallelism=${make_parallelism:-4}
+cur_dir="$(readlink -f "$(dirname "$0")")"
 docker_context_dir=$(
-  "$(dirname "$0")"/make_docker_context.py \
+  # The PYTHONPATH will go away in phase 2 of simplifying fbcode_builder usage
+  PYTHONPATH="$cur_dir:$PYTHONPATH" \
+  "$cur_dir/../bistro/build/ci/make_docker_context.py" \
     --os-image "$os_image" \
     --gcc-version "$gcc_version" \
     --make-parallelism "$make_parallelism" \
-    --option facebook/bistro:local_repo_dir "$(git rev-parse --show-toplevel)"
+    --option facebook/bistro:local_repo_dir "$cur_dir/.."
 )
 cd "${docker_context_dir?Failed to make Docker context directory}"
 docker build .

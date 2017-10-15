53b93d4ba0a53ddaf6ba4b2a10fbf9f268d98c61
Yedidya Feldblum <yfeldblum@fb.com>
2017-10-15 14:37:37 -0700

    CodeMod: Replace uses of wangle::IOThreadPoolExecutor
    Summary: CodeMod: Replace uses of `wangle::IOThreadPoolExecutor` with `folly::IOThreadPoolExecutor`, to which the former is merely an alias.
    
    Reviewed By: andrewjcg
    
    Differential Revision: D6005791
    
    fbshipit-source-id: 75763490f017e5df7c56d4f2a71be99e83edd5be

--- bistro/processes/TaskSubprocessQueue.h
+++ bistro/processes/TaskSubprocessQueue.h
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2015, Facebook, Inc.
+ *  Copyright (c) 2015-present, Facebook, Inc.
  *  All rights reserved.
  *
  *  This source code is licensed under the BSD-style license found in the
@@ -138,7 +138,7 @@ private:
   //  - constructor threw after spawning threads => abort() or leak threads
   //  - threads using partially-constructed object => heisenbugs
   //  - threads using partially-destructed members => heisenbugs
-  wangle::IOThreadPoolExecutor threadPool_;
+  folly::IOThreadPoolExecutor threadPool_;
 };
 
 namespace detail {

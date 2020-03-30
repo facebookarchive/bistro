6430d35e34a365a3b634c5f6a9907710367d6bd7
Andrew McFague <amcfague@fb.com>
2020-03-30 13:43:11 -0700

    Migrating fbcode/bistro/public_tld
    Summary: (Note: this ignores all push blocking failures!)
    
    Reviewed By: snarkmaster
    
    Differential Revision: D20651740
    
    fbshipit-source-id: bbf8bb23ffa9b96bd679a1032ab6302dd3a91822

--- build/fbcode_builder_config.py
+++ build/fbcode_builder_config.py
@@ -1,8 +1,5 @@
-#!/usr/bin/env python
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
+#!/usr/bin/env python3
+
 'fbcode_builder steps to build & test Bistro'
 
 import specs.fbthrift as fbthrift

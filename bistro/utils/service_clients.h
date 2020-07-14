91ba8155b77bad15bab16c244890398ad1a16bd9
Chad Austin <chadaustin@fb.com>
2020-07-14 13:29:02 -0700

    change AsyncSocket::newSocket to return a unique_ptr instead of shared_ptr
    Summary: If the caller wants a shared_ptr, UniquePtr will implicitly promote.
    
    Reviewed By: yfeldblum
    
    Differential Revision: D22046594
    
    fbshipit-source-id: 2e7d527d3ca33dae974c62db909df605c532ea44

--- bistro/utils/service_clients.h
+++ bistro/utils/service_clients.h
@@ -44,12 +44,12 @@ std::shared_ptr<ThriftClient> getAsyncClientForAddress(
     receive_timeout_ms = FLAGS_thrift_receive_timeout_ms;
   }
   using namespace apache::thrift;
-  auto socket = folly::AsyncSocket::newSocket(
+  auto socket = folly::to_shared_ptr(folly::AsyncSocket::newSocket(
     event_base,
     addr.ip_or_host,
     addr.port,
     connect_timeout_ms
-  );
+  ));
   auto channel = HeaderClientChannel::newChannel(socket);
   // This also sets the socket send timeout, but we overwrite it below.
   channel->setTimeout(receive_timeout_ms);

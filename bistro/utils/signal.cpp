a6710fc57be9ce7e0d076807977236b683dd11ff
Jim Meyering <meyering@fb.com>
2022-04-15 16:43:42 -0700

    bistro/bistro/utils/signal.cpp: use MT-safe sigdescr_np in place of now-removed sys_siglist
    Summary:
    This avoids the following errors:
    
      bistro/bistro/utils/signal.cpp:17:34: error: no member named 'sys_siglist' in the global namespace
      bistro/bistro/utils/signal.cpp:17:57: error: no member named 'sys_siglist' in the global namespace
      bistro/bistro/utils/signal.cpp:18:14: error: no member named 'sys_siglist' in the global namespace
    
    Differential Revision: D35693425
    
    fbshipit-source-id: 1d88496db93ff6c72209b206036618f0db0b380b

--- bistro/utils/signal.cpp
+++ bistro/utils/signal.cpp
@@ -8,15 +8,24 @@
 #include "bistro/bistro/utils/signal.h"
 
 #include <folly/Conv.h>
+#define _GNU_SOURCE 1
 #include <signal.h>
+#include <string.h>
 
 namespace facebook { namespace bistro {
 
 std::string describeSignal(int sig) {
+#if __cplusplus >= 202002L
+  // sigdescr_np is thread-safe, unlike strsignal().
+  auto desc = sigdescr_np(sig);
+  return desc ? desc : folly::to<std::string>("Unknown signal: ", sig);
+#else
+  // FIXME TODO: remove this else clause, once platform009 is gone.
   // sys_siglist should be thread-safe, unlike strsignal().
   if (sig >= 0 && sig < sizeof(::sys_siglist)/sizeof(*::sys_siglist)) {
     return ::sys_siglist[sig];
   }
+#endif
   return folly::to<std::string>("Unknown signal: ", sig);
 }
 
